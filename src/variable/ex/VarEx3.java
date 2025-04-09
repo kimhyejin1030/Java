650a5473b86378199c1a6c4ec45a0cdbff637c7c
kimhyejin1030 <k.hyejin1030@gmail.com>
2025-04-09 21:04:28 +0900

    First commit

---
+++ src/variable/ex/VarEx3.java
@@ -0,0 +1,12 @@
+package variable.ex;
+
+public class VarEx3 {
+
+    public static void main(String[] args) {
+        long first = 10000000000L;
+        System.out.println(first);
+
+        boolean second = true;
+        System.out.println(second);
+    }
+}
