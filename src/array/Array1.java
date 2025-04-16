caff3c7a8f97abf54e87bd00c62124245d58631f
kimhyejin1030 <k.hyejin1030@gmail.com>
2025-04-16 22:28:14 +0900

    Array

---
+++ src/array/Array1.java
@@ -0,0 +1,18 @@
+package array;
+
+public class Array1 {
+
+    public static void main(String[] args) {
+        int student1 = 90;
+        int student2 = 80;
+        int student3 = 70;
+        int student4 = 60;
+        int student5 = 50;
+
+        System.out.println("학생1 점수1: " + student1);
+        System.out.println("학생1 점수2: " + student2);
+        System.out.println("학생1 점수3: " + student3);
+        System.out.println("학생1 점수4: " + student4);
+        System.out.println("학생1 점수5: " + student5);
+    }
+}
